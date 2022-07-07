e902e895d6728e9471fd5298595a79f01e8267cc
Alexey Krukovich <aliaksei.krukovich@itechart-group.com>
2022-07-07 13:23:54 +0300

    refactor: move login resolver to query

--- modules/users/index.ts
+++ modules/users/index.ts
@@ -4,20 +4,20 @@ import { ENDPOINTS } from '../../constatns';
 
 export const usersMutations: {
   register: (_: null, data: User, context: IContext) => Promise<User>;
-  login: (_: null, data: { email: string; password: string }, context: IContext) => Promise<{ jwt: string }>;
 } = {
   register: async (_: null, data: User, context: IContext): Promise<User> => {
     return await http.post(ENDPOINTS.USERS.REGISTER, data, context);
-  },
-  login: async (_: null, data: { email: string; password: string }, context: IContext): Promise<{ jwt: string }> => {
-    return await http.post(ENDPOINTS.USERS.LOGIN, data, context);
   },
 };
 
 export const usersQuery: {
   getUserById: (_: null, data: { id: string }) => Promise<User>;
+  login: (_: null, data: { email: string; password: string }, context: IContext) => Promise<{ jwt: string }>;
 } = {
   getUserById: async (_: null, data: { id: string }): Promise<User> => {
     return await http.get(`${ENDPOINTS.USERS.GET}${data.id}`);
+  },
+  login: async (_: null, data: { email: string; password: string }, context: IContext): Promise<{ jwt: string }> => {
+    return await http.post(ENDPOINTS.USERS.LOGIN, data, context);
   },
 };
--- modules/users/schema.ts
+++ modules/users/schema.ts
@@ -3,11 +3,11 @@ import { gql } from 'apollo-server';
 export const typeUser = gql`
   type Query {
     getUserById(id: String!): User
+    login(email: String!, password: String!): Jwt
   }
 
   type Mutation {
     register(firstName: String!, lastName: String!, password: String!, email: String!): User
-    login(email: String!, password: String!): Jwt
   }
 
   type Jwt {
